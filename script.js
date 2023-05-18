2598ee49b866f41855c5704524e3b097c5693f73
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2023-05-18 20:02:51 -0300

    Exercício 5 feito

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const paletta = document.createElement('ul');
 const body = document.querySelector('body');
 const buttom = document.getElementById('button-random-color');
 
-// Requisito 2 e 3:
 function addCores(cor) {
   for (let indice = 0; indice < cor.length; indice += 1) {
     const linhaCor = document.createElement('li');
@@ -16,16 +15,32 @@ function addCores(cor) {
 addCores(cores);
 paletta.setAttribute('id', 'color-palette');
 
-// Requisito 4:
 paletta.appendChild(buttom);
-const ramdomColors = () => {
-  const colors = document.getElementsByClassName('color');
-  for (let index = 1; index < colors.length; index += 1) {
-    const cor1 = Math.floor(Math.random() * 256);
-    const cor2 = Math.floor(Math.random() * 256);
-    const cor3 = Math.floor(Math.random() * 256);
-    const cor = `rgb(${cor1}, ${cor2}, ${cor3})`;
-    colors[index].style.backgroundColor = cor;
+const ramdomColors = (event) => {
+  let colors = document.getElementsByClassName('color');
+  console.log(event);
+  let array = [];
+  for (let index = 0; index < colors.length; index += 1) {
+    if (index !== 0) {
+      if (!event[index]) {
+        let cor1 = Math.floor(Math.random() * 256);
+        let cor2 = Math.floor(Math.random() * 256);
+        let cor3 = Math.floor(Math.random() * 256);
+        let color = `rgb(${cor1}, ${cor2}, ${cor3})`;
+        colors[index].style.backgroundColor = color;
+        array.push({index, color});
+      localStorage.setItem('colorPalette', JSON.stringify(array));
+      }else {
+        colors[index].style.backgroundColor = event[index].color;
+      }
+    } else {
+      array.push({index, color: 'rgb(0, 0, 0)'})
+    }
   }
 };
 buttom.addEventListener('click', ramdomColors);
+const coresFixadas = localStorage.getItem('colorPalette');
+console.log();
+if (coresFixadas) {
+  ramdomColors(JSON.parse(coresFixadas));
+}
